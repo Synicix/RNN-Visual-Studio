94b09e479763d5871c448537ce0957c9f01dc734
Synicix <Synicix@gmail.com>
2017-11-14 16:44:44 -0600

    Revert "Update"
    This reverts commit 6fbbd4cf38b7327255bfab08e400f998f49ccdcf.

--- RNN/RNN.py
+++ RNN/RNN.py
@@ -5,14 +5,19 @@ import numpy as np
 import random
 
 # RNN Hyper Parameters
+<<<<<<< HEAD
 alphabet_size = 96
 batch_size = 100000
 num_of_hidden_states = 100
 num_of_steps = 100
+=======
+alphabet_size = 127
+batch_size = 5
+num_of_hidden_states = 10
+num_of_steps = 1
+>>>>>>> parent of 6fbbd4c... Update
 learning_rate = 0.1
-iterations = 2000
-
-
+num_of_epocs = 10
 
 def parseTextFile(file_path):
     words = []
@@ -24,40 +29,32 @@ def parseTextFile(file_path):
         for line in fp:
             char_array = list(line)
             for char in char_array: # For every Char
-                if char == "\n":
-                    words.append(95)
-                    labels.append(95)
-                else:
-                    words.append(ord(char) - 32)
-                    labels.append(ord(char) - 32)
-               
-    
+                words.append(ord(char))
+                labels.append(ord(char))
+
 
     labels.pop(1)
-    labels.append(0)
 
     return words, labels
 
 
-def getBatch(words, labels):
+def getBatch(words, lables):
     temp_words = [[0 for x in range(num_of_steps)] for y in range(batch_size)]
     temp_lables = [[0 for x in range(num_of_steps)] for y in range(batch_size)]
+
     current_letter = 0
     for i in range(0, batch_size):
         current_letter = random.randint(0, words.__len__())
         for j in range(0, num_of_steps):
             temp_words[i][j] = words[current_letter]
-            temp_lables[i][j] = labels[current_letter]
+            temp_lables[i][j] = lables[current_letter]
+            print(words[current_letter])
             current_letter = (current_letter + 1) % words.__len__()
 
     return temp_words, temp_lables
 
 
-def build_graph(words, labels_key):
-    session = tf.InteractiveSession()
-
-
-
+def build_graph(words, labels_key, session):
     # Input
     with tf.name_scope("Inputs"):
         inputs = tf.placeholder(tf.int32, [None, num_of_steps], name="inputs")
@@ -65,19 +62,19 @@ def build_graph(words, labels_key):
 
     rnn_inputs = tf.one_hot(inputs, 127, 1.0, 0.0, -1)
 
-    # LSTM
+    # RNN
+    init_state = tf.zeros([batch_size, num_of_hidden_states])
 
-    with tf.name_scope("LSTM_Cell"):
-        rnn_cell = tf.contrib.rnn.BasicLSTMCell(num_of_hidden_states)
-        rnn_outputs, final_state = tf.nn.dynamic_rnn(rnn_cell, rnn_inputs, dtype=tf.float32)
- 
+    with tf.name_scope("RNN_Cell"):
+        rnn_cell = tf.contrib.rnn.BasicRNNCell(num_of_hidden_states)
+        rnn_outputs, final_state = tf.nn.dynamic_rnn(rnn_cell, rnn_inputs, initial_state=init_state)
+
+
 
     # Loss and Training Step
     with tf.variable_scope("Cell-Output"):
         W_y = weight_variable([num_of_hidden_states, alphabet_size ], "W_y")
         b_y = bias_variable([alphabet_size], "b_y")
-        tf.summary.histogram("W_y", W_y)
-        tf.summary.histogram("b_y", b_y)
         
     with tf.name_scope("Loss"):
         logits = tf.reshape(tf.matmul(tf.reshape(rnn_outputs, [-1, num_of_hidden_states]), W_y) + b_y, [batch_size, num_of_steps, alphabet_size])
@@ -89,6 +86,7 @@ def build_graph(words, labels_key):
 
         tf.summary.scalar("loss", total_loss)
 
+<<<<<<< HEAD
         summary = tf.summary.merge_all()
         writer = tf.summary.FileWriter("/TensorBoard/RNN", session.graph)
 
@@ -113,7 +111,17 @@ def build_graph(words, labels_key):
 
         save_path = saver.save(session, "/RNN/model.ckpt")
 
+=======
+>>>>>>> parent of 6fbbd4c... Update
 
+def train_network(iterations, summary, writer, session):
+    for i in range(iterations):
+        session.run(tf.global_variables_initializer())
+        session.run(train_step, feed_dict={inputs: words, labels: labels_key})
+        if i % 100 == 0:
+            print("Step: " + str(i))
+            s = session.run(summary, feed_dict={inputs: words, labels: labels_key})
+            writer.add_summary(s, i)
 
 
 
@@ -130,8 +138,14 @@ def main():
     words, labels = parseTextFile("input.txt")
     batched_words, batched_labels = getBatch(words, labels)
 
-    train_step = build_graph(batched_words, batched_labels)
- 
+    session = tf.InteractiveSession()
+
+    build_graph(batched_words, batched_labels, session)
+    summary = tf.summary.merge_all()
+    writer = tf.summary.FileWriter("/TensorBoard/RNN", session.graph)
+
+    train_network(20, summary, writer, session)
+
 
 
 if __name__ == "__main__":
